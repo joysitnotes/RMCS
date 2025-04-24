6f8a2529bf847f12c357078979e8fab81b2d0944
JoysgitHub <114746470+JoysgitHub@users.noreply.github.com>
2025-04-24 04:25:20 +0100

    Create setup.sh

---
+++ RTSPCAM/setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Check if mediamtx is in PATH
+if which mediamtx > /dev/null 2>&1; then
+    echo "[+] Mediamtx is installed at: $(which mediamtx)"
+else
+    echo "[*] Installing Mediamtx"
+    wget https://github.com/bluenviron/mediamtx/releases/download/v1.12.0/mediamtx_v1.12.0_linux_armv7.tar.gz
+    tar -xvzf mediamtx_v1.12.0_linux_armv7.tar.gz
+    cd mediamtx
+    sudo mv mediamtx /usr/local/bin/
+    sudo chmod +x /usr/local/bin/mediamtx
+    
+    if which ffmpeg > /dev/null 2>&1; then
+        echo echo "[+] Mediamtx is installed at: $(which mediamtx)"
+    else
+        echo "[-] Mediamtx installation failed."
+    fi
+
+fi
+
+
+
+if which ffmpeg > /dev/null 2>&1; then
+    echo "[+] FFmpeg is already installed: $(ffmpeg -version | head -n 1)"
+else
+    echo "[*] FFmpeg not found. Installing"
+    sudo apt update
+    sudo apt install ffmpeg -y
+    if which ffmpeg > /dev/null 2>&1; then
+        echo "[+] FFmpeg installed successfully."
+    else
+        echo "[-] FFmpeg installation failed."
+    fi
+fi

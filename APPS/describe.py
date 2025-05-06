c0de539b257917634f0245597dba94da003b817a
JoysgitHub <114746470+JoysgitHub@users.noreply.github.com>
2025-05-06 23:06:56 +0100

    Update describe.py
    restructured to allow faster notifications

--- APPS/describe.py
+++ APPS/describe.py
@@ -7,9 +7,9 @@ import sys
 from datetime import datetime
 from pushbullet import Pushbullet
 import mimetypes
+import torch
 
-# Add pushbullet api token
-pb = Pushbullet("API_TOKEN")
+pb = Pushbullet("APIKEY")
 now = datetime.now()
 
 if len(sys.argv) != 4:
@@ -20,50 +20,8 @@ else:
     VOICE = int(sys.argv[2])
     NOTIFY = int(sys.argv[3])
 
-warnings.filterwarnings("ignore", message="Using a slow image processor as `use_fast` is unset")
-
-# Load the model and processor
-processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base", use_fast=True)
-model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
-
-# Function to generate a detailed caption for a local image
-def generated_caption(image_path):
-    try:
-        image = Image.open(image_path)
-
-        
-        inputs = processor(image, return_tensors="pt")
-        out = model.generate(**inputs, max_length=100)  
-        caption = processor.decode(out[0], skip_special_tokens=True)
-
-        
-
-        return caption
-    except Exception as e:
-        return f"An error occurred: {e}"
-
-caption = generated_caption(IMAGEPATH)
-time = now.strftime("%H:%M %p")
-full_caption = f"Warning On Camera 1 at {time} there is {caption}"
-
-print(f"{full_caption}")
-
-
-
-# Speech 
-if VOICE == 1:
-    print("[+] Voice Active")
-    engine = pyttsx3.init()
-    engine.setProperty('rate', 150)   
-    engine.setProperty('volume', 1)     
-    engine.say(full_caption)
-    engine.runAndWait()
-
-# Notifications
 if NOTIFY == 1:
-
-
-    print("[+] Sending Notification")
+    print("[+] Sending Image Notification")
     mime_type, _ = mimetypes.guess_type(IMAGEPATH)
 
     with open(IMAGEPATH, "rb") as f:
@@ -72,5 +30,56 @@ if NOTIFY == 1:
     pb.push_file(file_name=file_data["file_name"],
                  file_url=file_data["file_url"],
                  file_type=mime_type,
-                 body=full_caption,
-                 title="WARNING: ")
+                 body=" ",
+                 title="WARNING")
+
+
+warnings.filterwarnings("ignore", message="Using a slow image processor as `use_fast` is unset")
+
+
+processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base", use_fast=True)
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
+
+def generated_caption(image_path):
+    try:
+        
+        image = Image.open(image_path)
+
+     
+        inputs = processor(image, return_tensors="pt").to(device)
+        out = model.generate(**inputs, max_length=100)  
+        caption = processor.decode(out[0], skip_special_tokens=True)
+
+        return caption
+    except Exception as e:
+        return f"An error occurred: {e}"
+
+caption = generated_caption(IMAGEPATH)
+
+time = now.strftime("%H:%M %p")
+full_caption = f"Warning On Camera 1 at {time} there is {caption}"
+
+print(f"{full_caption}")
+
+
+if NOTIFY == 1:
+    print("[+] Sending Text Notification")
+    pb.push_note(title=" ", body=full_caption)
+
+
+
+if VOICE == 1:
+    print("[+] Voice Active")
+    engine = pyttsx3.init()
+
+    engine.setProperty('rate', 150)    
+    engine.setProperty('volume', 1)     
+  
+    engine.say(full_caption)
+    engine.runAndWait()
+
+
